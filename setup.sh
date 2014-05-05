91a100f8466d6cc179d28e4f333ee7135e675df0
Zafar Takhirov <z.tahirov@gmail.com>
2014-05-04 22:50:38 -0700

    Initial Creation
    The initial files are taken from Balaji S. Srinivasan as part of
    Stanford “StartUp” course.

---
+++ setup.sh
@@ -0,0 +1,14 @@
+
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+
+ln -s dotfiles/.screenrc .
+ln -s dotfiles/.bash_profile .
+ln -s dotfiles/.bashrc .
+ln -s dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
