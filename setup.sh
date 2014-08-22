c459ddb3e8fa74a1b70656edbab8b153d66c1f0c
Zafar Takhirov <z.tahirov@gmail.com>
2014-08-22 11:11:10 -0700

    Added support for GitHUB

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
+#!/usr/bin/env bash
 
+WARNINGMSG="
+\t\e[91m!!!WARNING!!!\e[0m\n
+This script is \e[91mDESTRUCTIVE!\e[0m It overwrites\n
+all your previous (dotfile) setup.\n
+Press <Ctrl+C> NOW to cancel..."
 
-SETUPDIR=`pwd` 
+echo -e "${WARNINGMSG}"
+read cancel
+
+SETUPDIR=`pwd`
 cd ${HOME}
 if [ -d ./dotfiles/ ]; then
     mv dotfiles dotfiles.old
@@ -21,3 +30,33 @@ ln -sv 	${SETUPDIR}/dotfiles/.bash_profile 	${HOME}/.bash_profile
 ln -sv 	${SETUPDIR}/dotfiles/.bashrc 		${HOME}/.bashrc
 ln -sv 	${SETUPDIR}/dotfiles/.bashrc_custom 	${HOME}/.bashrc_custom
 ln -svf	${SETUPDIR}/dotfiles/.emacs.d 		${HOME}/.emacs.d
+
+
+# GIT:
+if [-d ${HOME}/.git.OLD/ ]; then
+    rm -rfv ${HOME}/.git.OLD
+fi
+mkdir ${HOME}/.git.OLD
+
+# move .gitignore:
+mv -v ${HOME}/.gitignore ${HOME}/.git.OLD/
+ln -sv	${SETUPDIR}/dotfiles/.gitignore		${HOME}/.gitignore
+
+# create .gitconfig:
+mv -v ${HOME}/.gitconfig ${HOME}/.git.OLD/
+GITCONFIG="[include]
+\tpath = ${SETUPDIR}/dotfiles/.gitconfig"
+echo -e "${GITCONFIG}" > ${HOME}/.gitconfig
+
+# Create GIT user data:
+GITUSER="[user]
+\tname = \"\"
+\temail = \"\"
+[github]
+\tuser = 
+\ttoken = "
+if [ ! -f ${HOME}/.gitconfig_user ]; then
+    echo -e "${GITUSER}" > ${HOME}/.gitconfig_user
+else
+	echo -e ".gitconfig_user exists!\n"
+fi
