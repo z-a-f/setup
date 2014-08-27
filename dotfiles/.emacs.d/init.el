704e2607f8552eb0f761555bdf7b3f58e0858ebe
Zafar Takhirov <z.tahirov@gmail.com>
2014-08-26 18:08:42 -0700

    AUTOCOMMIT: Tue Aug 26 18:08:42 PDT 2014

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -162,4 +162,5 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-enabled-themes (quote (deeper-blue)))
+)
 
