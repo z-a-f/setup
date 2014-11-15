7e57097f241d6c65c19eb99f5650db88aff78bb1
Zafar Takhirov <z.tahirov@gmail.com>
2014-11-14 21:28:29 -0800

    edited emacs

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -30,6 +30,20 @@
 (setq suggest-key-bindings t)
 (setq vc-follow-symlinks t)
 
+;; ------------------------
+;; -- Enable XTERM mouse --
+;; ------------------------
+(unless (string-equal system-type "windows-nt")
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] 
+		  '(lambda() (interactive) (scroll-down 1)))
+  (global-set-key [mouse-5]
+		  '(lambda() (interactive) (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+)
+
 ;; --------------------------
 ;; -- Custom Set Variables --
 ;; --------------------------
