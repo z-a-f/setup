c85bcadf7242e333e97039fe9b6574ad1f5ea105
Zafar Takhirov <z.tahirov@gmail.com>
2015-02-03 14:23:08 -0800

    Fix for emacs background

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -202,6 +202,7 @@
 ;; --------------------
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+(custom-set-faces (if (not window-system) '(default ((t (:background "nil"))))))
 
 ;; ---------------
 ;; -- Mac Stuff --
